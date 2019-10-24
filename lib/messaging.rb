0efdbcab24875f59848e8862a000e131a29d5d32
Adrià Cidre <adria.cidre@gmail.com>
2019-10-24 15:19:33 +0200

    Aviod waiting on first reconnection

--- lib/messaging.rb
+++ lib/messaging.rb
@@ -190,9 +190,12 @@ module Selfid
         end
 
         @ws.on :close do |event|
-          Selfid.logger.info "websocket connection closed (#{event.code}) #{event.reason}"
-          sleep 10
-          Selfid.logger.info "reconnecting..."
+          if not @reconnection_delay.nil?
+            Selfid.logger.info "websocket connection closed (#{event.code}) #{event.reason}"
+            sleep @reconnection_delay
+            Selfid.logger.info "reconnecting..."
+          end
+          @reconnection_delay = 3
           start_connection
         end
       end
