1850efac93ad157cbd78ea127a773817d936fa75
Adrià Cidre <adria.cidre@gmail.com>
2019-10-24 15:05:18 +0200

    Improve time sync

--- lib/messaging.rb
+++ lib/messaging.rb
@@ -12,7 +12,7 @@ require_relative 'proto/aclcommand_pb'
 
 module Selfid
   class MessagingClient
-    attr_accessor :inbox, :client, :jwt, :app_id, :device_id
+    attr_accessor :inbox, :client, :jwt, :device_id
 
     # RestClient initializer
     #
