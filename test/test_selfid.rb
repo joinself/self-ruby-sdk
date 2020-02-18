6ebae71e8fe7d728028d6ab61115a9cb81ad9d7f
Adrià Cidre <adria.cidre@gmail.com>
2020-02-18 10:33:52 +0100

    fix tets

--- test/test_selfid.rb
+++ test/test_selfid.rb
@@ -33,21 +33,21 @@ class SelfidTest < Minitest::Test
     end
 
     def test_init_with_defaults
-      assert_equal "https://api.review.selfid.net", app.client.self_url
+      assert_equal "https://api.selfid.net", app.client.self_url
       assert_equal app_id, app.jwt.id
       assert_equal seed, app.jwt.key
     end
 
     def test_init_with_custom_parameters
-      custom_app = Selfid::App.new(app_id, seed, self_url: "http://custom.self.net", messaging_url: nil)
+      custom_app = Selfid::App.new(app_id, seed, base_url: "http://custom.self.net", messaging_url: nil)
       assert_equal "http://custom.self.net", custom_app.client.self_url
       assert_equal app_id, custom_app.jwt.id
       assert_equal seed, custom_app.jwt.key
     end
 
     def test_authenticate
-      body = "{\"payload\":\"eyJkZXZpY2VfaWQiOiIxIiwidHlwIjoiYXV0aGVudGljYXRpb25fcmVxIiwiYXVkIjoiaHR0cHM6Ly9hcGkucmV2aWV3LnNlbGZpZC5uZXQiLCJpc3MiOiJvOW1wbmc5bTJqdiIsInN1YiI6Inh4eHh4eHh4IiwiaWF0IjoiMjAxOS0wOS0wMVQxMDowNTowMFoiLCJleHAiOiIyMDE5LTA5LTAxVDExOjA1OjAwWiIsImNpZCI6InV1aWQiLCJqdGkiOiJ1dWlkIn0\",\"protected\":\"eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9\",\"signature\":\"OeFdBn6pp2ncr2bmbH-d6doBWh8EsaRYzWU8h611lJWhoxZ6lnxgxzVEqkK9WVcqzyVpVTSO-0y0gaPWu8pbDw\"}"
-      stub_request(:post, "https://api.review.selfid.net/v1/auth").
+      body = "{\"payload\":\"eyJkZXZpY2VfaWQiOiIxIiwidHlwIjoiYXV0aGVudGljYXRpb25fcmVxIiwiYXVkIjoiaHR0cHM6Ly9hcGkuc2VsZmlkLm5ldCIsImlzcyI6Im85bXBuZzltMmp2Iiwic3ViIjoieHh4eHh4eHgiLCJpYXQiOiIyMDE5LTA5LTAxVDEwOjA1OjAwWiIsImV4cCI6IjIwMTktMDktMDFUMTE6MDU6MDBaIiwiY2lkIjoidXVpZCIsImp0aSI6InV1aWQifQ\",\"protected\":\"eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9\",\"signature\":\"9IrKkOUxUsEpQeR-7QR1wYzmgglkrl8XX_sqixZMONOSqXw0QCKr7zy_YyOpob-Lq5rHsYbIE7j1tz-w1ee2Ag\"}"
+      stub_request(:post, "https://api.selfid.net/v1/auth").
         with(body: body, headers: headers).
         to_return(status: 200, body: "", headers: {})
 
@@ -58,12 +58,25 @@ class SelfidTest < Minitest::Test
       pk = "pk_111222333"
       id = "111222333"
 
-      stub_request(:get, "https://api.review.selfid.net/v1/apps/#{id}").
+      stub_request(:get, "https://api.selfid.net/v1/identities/#{id}").
         with(headers: headers).
         to_return(status: 200, body: '{"public_keys":[{"id":"1","key":"' + pk + '"}]}', headers: {})
 
       identity = app.identity(id)
       assert_equal pk, identity[:public_keys].first[:key]
     end
+
+    def test_app
+      pk = "pk_111222333"
+      id = "111222333"
+
+      stub_request(:get, "https://api.selfid.net/v1/apps/#{id}").
+        with(headers: headers).
+        to_return(status: 200, body: '{"public_keys":[{"id":"1","key":"' + pk + '"}]}', headers: {})
+
+      a = app.app(id)
+      assert_equal pk, a[:public_keys].first[:key]
+    end
+
   end
 end
