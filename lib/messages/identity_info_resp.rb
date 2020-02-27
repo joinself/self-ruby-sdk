730f50b1c325a22d1cdeb2b9642706424dba010e
Tom Bevan <tom.bevan@r3labs.io>
2020-02-27 12:08:18 +0000

    debug

--- lib/messages/identity_info_resp.rb
+++ lib/messages/identity_info_resp.rb
@@ -15,6 +15,9 @@ module Selfid
         @input = input
         @typ = MSG_TYPE
         @payload = get_payload input
+
+        p payload
+
         @id = payload[:cid]
         @from = payload[:iss]
         @to = payload[:sub]
