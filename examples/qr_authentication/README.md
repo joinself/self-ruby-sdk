0c59d32c193f17ce0f3c5cb1675f657caa612f8d
Adrià Cidre <adria.cidre@gmail.com>
2020-03-23 14:16:26 +0100

    QR auth example based on web browser

--- examples/qr_authentication/README.md
+++ examples/qr_authentication/README.md
@@ -1,6 +1,6 @@
 # QR based authentication request
 
-A Self user will be authenticated on your platform by scanning a QR code and accepting the authentication request on his phone. 
+A Self user will be authenticated on your platform by scanning a QR code and accepting the authentication request on his phone.
 
 As part of this process, you have to share the generated QR code with your users, and wait for a response
 
@@ -11,7 +11,14 @@ In order to run this example, you must have a valid app keypair. Self-keypairs a
 Once you have your valid `SELF_APP_ID` and `SELF_APP_SECRET` you can run this example with:
 
 ```bash
-$ SELF_APP_ID=XXXXX SELF_APP_SECRET=XXXXXXXX ruby app.rb
+$ bundle install
+$ SELF_APP_ID=XXXXX SELF_APP_SECRET=XXXXXXXX rackup
+```
+
+Open your browser on http://127.0.0.1:9292/ and scan the presented code. Once you accept the request on your phone check your command line for a line like.
+
+```
+ User is now authenticated 🤘
 ```
 
 ## Process diagram
--- examples/qr_authentication/app.rb
+++ examples/qr_authentication/app.rb
@@ -1,44 +1,47 @@
 # frozen_string_literal: true
 
+require 'sinatra'
 require 'rqrcode'
 require 'selfid'
 
-# Disable the debug logs
-Selfid.logger = Logger.new('/dev/null')
+class App < Sinatra::Base
+  configure do
+    # Disable the debug logs
+    # Selfid.logger = Logger.new('/dev/null')
 
-# Connect your app to Self network, get your connection details creating a new
-# app on https://developer.selfid.net/
-@app = Selfid::App.new(ENV["SELF_APP_ID"], ENV["SELF_APP_SECRET"])
-# Allows connections from everyone on self network to your app.
-@app.permit_connection("*")
+    # Connect your app to Self network, get your connection details creating a new
+    # app on https://developer.selfid.net/
+    app = Selfid::App.new(ENV["SELF_APP_ID"], ENV["SELF_APP_SECRET"],
+    base_url: "https://api.review.selfid.net", messaging_url:"wss://messaging.review.selfid.net/v1/messaging")
 
-# Register an observer for an authentication response
-@app.on_message Selfid::Messages::AuthenticationResp::MSG_TYPE do |auth|
-  # The user has rejected the authentication
-  if not auth.accepted?
-    puts "Authentication request has been rejected"
-    exit!
+    # Allows connections from everyone on self network to your app.
+    app.permit_connection("*")
+
+    # Register an observer for an authentication response
+    app.on_message Selfid::Messages::AuthenticationResp::MSG_TYPE do |auth|
+      # The user has rejected the authentication
+      if not auth.accepted?
+        puts "Authentication request has been rejected"
+        exit!
+      end
+
+      puts "User is now authenticated 🤘"
+      exit
+    end
+
+    set :client, app
   end
 
-  puts "User is now authenticated 🤘"
-  exit
+  get '/' do
+    # Print a QR code to authenticate
+    req = settings.client.authenticate("-", request: false)
+
+    # Share resulting image with your users
+    png = RQRCode::QRCode.new(req, :level => 'l').as_png(
+      border: 0,
+      size: 400
+    ).save('public/qr.png', :interlace => true)
+
+    erb :index
+  end
 end
-
-# Print a QR code to authenticate
-req = @app.authenticate("-", request: false)
-
-# Share resulting image with your users
-png = RQRCode::QRCode.new(req, :level => 'l').as_png(
-  border: 0,
-  size: 400
-)
-IO.binwrite("/tmp/qr.png", png.to_s)
-
-# This will open the exported qr.png with your default software,
-# manually open /tmp/qr.png and scan it with your device if it
-# does not work
-p "Scan /tmp/qr.png with your device"
-`open /tmp/qr.png`
-
-# Wait for some time
-sleep 100
