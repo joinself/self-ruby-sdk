915ea7b155f942741f46965ada15833c23ac73eb
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2020-05-04 16:16:49 +0200

    source instead of sources on intermediary example

--- examples/intermediary_request/app.rb
+++ examples/intermediary_request/app.rb
@@ -18,7 +18,7 @@ opts = ENV.has_key?('SELF_BASE_URL') ? { base_url: ENV["SELF_BASE_URL"], messagi
 # without ever leaking information about the user's fact.
 res_opts = {}
 res_opts[:intermediary] = ENV['SELF_INTERMEDIARY'] if ENV.has_key?('SELF_INTERMEDIARY')
-res = @app.facts.request_via_intermediary(user, [{ source: Selfid::SOURCE_USER_SPECIFIED,
+res = @app.facts.request_via_intermediary(user, [{ sources: [Selfid::SOURCE_USER_SPECIFIED],
                                                          fact: Selfid::FACT_EMAIL,
                                                          operator: '==',
                                                          expected_value: 'test@test.org' }], res_opts)
