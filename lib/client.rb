2a9de4d78bb4ed89b25b64e211df2fa86b57e64a
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2020-09-03 17:16:01 +0200

    print api output

--- lib/client.rb
+++ lib/client.rb
@@ -70,6 +70,7 @@ module SelfSDK
     # @param id [string] identity id
     def public_key(id, kid)
       i = entity(id)
+      p i
       sg = SelfSDK::SignatureGraph.new(i[:history])
       sg.key_by_id(kid)
     end
