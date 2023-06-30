08774d003579ce60d31a0b4b30d567721cdea5b9
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2023-06-30 11:09:38 +0200

    test script

--- tmp/review_problem/main.rb
+++ tmp/review_problem/main.rb
@@ -11,33 +11,36 @@ def setup(app_id, device_id, storage_key)
 
   # Connect your app to Self network, get your connection details creating a new
   # app on https://developer.selfsdk.net/
-  app = SelfSDK::App.new(app_id, 
-                         device_id, 
-                         storage_key, 
-                         storage_dir,
-                         opts).start
-  app.messaging.permit_connection("*")
-  sleep 1
-  app
+  SelfSDK::App.new(app_id, 
+                   device_id, 
+                   storage_key, 
+                   storage_dir,
+                   opts)
 end
 
 puts "1. start app sdk &&  2. publish app prekeys"
-oldapp = setup(ENV["SELF_APP_ID"], ENV["SELF_APP_DEVICE_SECRET"], ENV["STORAGE_KEY"])
+oldapp = setup(ENV["SELF_APP_ID"], ENV["SELF_APP_DEVICE_SECRET"], ENV["STORAGE_KEY"]).start
 
 puts "3. close app sdk"
 oldapp.close
 sleep 5
 
 puts "4. start app sdk"
-newapp = setup(ENV["SELF_APP_ID"], ENV["SELF_APP_DEVICE_SECRET"], ENV["STORAGE_KEY"])
-
-puts "5. start client sdk"
-client = setup(ENV["CLIENT_SELF_APP_ID"], ENV["CLIENT_SELF_APP_DEVICE_SECRET"], ENV["STORAGE_KEY"])
-
-newapp.chat.on_message do |msg|
-  puts "7. app decrypt message"
+@newapp = setup(ENV["SELF_APP_ID"], ENV["SELF_APP_DEVICE_SECRET"], ENV["STORAGE_KEY"])
+sleep 5
+@newapp.chat.on_message do |msg|
+  puts "......."
+  puts "......."
+  puts "7. app decrypt message #{msg.body}"
+  puts "......."
+  puts "......."
   puts msg.body
 end
+@newapp.start
+# TODO: if I move the subscription to after the start, it does not work WTF!!!!!
+
+puts "5. start client sdk"
+client = setup(ENV["CLIENT_SELF_APP_ID"], ENV["CLIENT_SELF_APP_DEVICE_SECRET"], ENV["STORAGE_KEY"]).start
 
 sleep 5
 puts "6. client create session with app"
